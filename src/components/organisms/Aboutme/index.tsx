988374c60cfa5bb43e3bfbf92e5e9984e7aaa899
Jonatan Luis Moreno <jonatanluismoreno@gmail.com>
2022-05-27 21:24:20 -0300

    add Aboutme Component

---
+++ src/components/organisms/Aboutme/index.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import styled from "styled-components";
+
+interface Props {
+  title: string;
+  description: string;
+}
+
+const StyledAboutme = styled.div`
+  text-align: initial;
+  width: 60vw;
+  margin-top: 205px;
+  @media (max-width: 1750px) {
+    width: calc(100vw - 500px);
+    margin-left: 100px;
+    margin-right: 100px;
+  }
+  @media (max-width: 1150px) {
+    width: 70vw;
+  }
+`;
+
+const StyledAboutmeTitle = styled.h3`
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 26px;
+  letter-spacing: 0.04em;
+  color: blue;
+`;
+
+const StyledAboutmeDescriptionContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledAboutmeDescription = styled.p`
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 26px;
+  letter-spacing: 0.04em;
+  color: blue;
+  :after {
+    display: inline-block;
+    position: absolute;
+    width: 60px;
+    height: 5px;
+    left: 360px;
+    background: black;
+    border-radius: 2px;
+    transform: rotate(-90deg);
+    z-index: 2;
+  }
+`;
+
+export default function Skill({ title, description }: Props) {
+  return (
+    <StyledAboutme>
+      <StyledAboutmeTitle>{title}</StyledAboutmeTitle>
+      <StyledAboutmeDescriptionContainer>
+        <StyledAboutmeDescription>{description}</StyledAboutmeDescription>
+      </StyledAboutmeDescriptionContainer>
+    </StyledAboutme>
+  );
+}
