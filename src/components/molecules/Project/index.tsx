3392ce0daf8c2092d5b6ea7604c20e9d2bfa4786
Jonatan Luis Moreno <jonatanluismoreno@gmail.com>
2022-06-14 20:33:25 -0300

    add Project Component

---
+++ src/components/molecules/Project/index.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import styled from "styled-components";
+import Carousel from "../../atoms/Carousel";
+
+interface Props {
+  listImages: any;
+  seconds: number;
+  tecnologies: string[];
+}
+
+const StyledProject = styled.div`
+  background: #222525;
+  border-radius: 6px;
+  width: 27vw;
+  margin: 0;
+  @media (max-width: 1300px) {
+    width: 50vw;
+  }
+  @media (max-width: 1150px) {
+    margin-bottom: 40px;
+  }
+`;
+
+const StyledProjectBotton = styled.div`
+  @media (max-width: 1150px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+const StyledProjectTitle = styled.div`
+  margin-left: 25px;
+  margin-top: 30px;
+  margin-bottom: 10px;
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 24px;
+  color: #d9f2f2;
+`;
+const StyledProjectTechnologies = styled.div`
+  margin-left: 25px;
+  display: flex;
+  gap: 10px;
+  @media (max-width: 1150px) {
+    margin-left: 0;
+  }
+`;
+const StyledProjectTechnology = styled.span`
+  background: linear-gradient(90deg, #00f5a0 0%, #00d9f5 100%);
+  border-radius: 4px;
+  padding: 8px 16px;
+  font-family: "Montserrat";
+  font-style: normal;
+  letter-spacing: 0.12em;
+  font-weight: 400;
+  font-size: 10px;
+  line-height: 14px;
+  }
+`;
+const StyledProjectDescription = styled.p`
+  margin-left: 25px;
+  margin-top: 25px;
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: 0.04em;
+  color: #bdebea;
+  @media (max-width: 1150px) {
+    text-align: center;
+  }
+`;
+export default function Project({ listImages, seconds, tecnologies }: Props) {
+  return (
+    <StyledProject>
+      <Carousel carouselItems={listImages} seconds={seconds} />
+      <StyledProjectBotton>
+        <StyledProjectTitle>FRUTTI STOP</StyledProjectTitle>
+        <StyledProjectTechnologies>
+          {tecnologies.map((technology: string) => (
+            <StyledProjectTechnology>{technology}</StyledProjectTechnology>
+          ))}
+        </StyledProjectTechnologies>
+        <StyledProjectDescription>
+          A tipical game where you have to complete a list of topics with
+          randoms letters. Its multiplayer and you can play with your friends.
+        </StyledProjectDescription>
+      </StyledProjectBotton>
+    </StyledProject>
+  );
+}
