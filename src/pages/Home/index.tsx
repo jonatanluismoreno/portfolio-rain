2829cc6dbe38484c0c8e9178d4cb5b09658fc9c0
Jonatan Luis Moreno <jonatanluismoreno@gmail.com>
2022-06-14 20:32:29 -0300

    add Home Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,11 +3,13 @@ import styled from "styled-components";
 import Main from "../../components/organisms/Main";
 import Header from "../../components/organisms/Header";
 import Aboutme from "../../components/organisms/Aboutme";
+import Projects from "../../components/organisms/Projects";
 import Skills from "../../components/organisms/Skills";
+import Footer from "../../components/organisms/Footer";
 import arrowscroll from "../../assets/images/arrowscroll.svg";
 
 const StyledHome = styled.section`
-  @keyframes example {
+  @keyframes fadein {
     from {
       top: 0px;
     }
@@ -24,13 +26,13 @@ const StyledHome = styled.section`
 const StyledHomeImage = styled.img`
   position: relative;
   display: inline-block;
-  animation-name: example;
+  animation-name: fadein;
   animation-duration: 2s;
   animation-iteration-count: infinite;
   animation-direction: alternate;
 `;
 
-export default function Home() {
+export default function HomePage() {
   const goToAboutMe = () => {
     window.scrollTo({
       top: 900,
@@ -54,7 +56,9 @@ export default function Home() {
           improve my skills. I'm a self-motivated person who is always looking
           for new challenges and opportunities."
       />
+      <Projects />
       <Skills />
+      <Footer />
     </StyledHome>
   );
 }
