15de82e58658d6cea5a6fbd1f8a90d151b61da93
Jonatan Luis Moreno <jonatanluismoreno@gmail.com>
2022-06-14 20:33:37 -0300

    add Card Component

---
+++ src/components/molecules/Card/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+interface Props {
+  text: string;
+  // sizes?: "small" | "medium" | "large";
+}
+
+const StyledButton = styled.button`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20px;
+  color: c.$black40;
+  padding: 12px 20px 13px;
+  background: c.$linear10;
+  border-radius: 6px;
+`;
+
+export default function Card({ text }: Props) {
+  return <StyledButton type="button">{text}</StyledButton>;
+}
+
+/* Button.defaultProps = {
+  sizes: "medium"
+};
+ */
